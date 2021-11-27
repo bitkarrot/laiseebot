d2e98b0d4423a942d2ca7cf28301169310342853
Bitcarrot <73979971+bitkarrot@users.noreply.github.com>
2021-11-26 19:52:07 -0800

    update

--- constants.py
+++ constants.py
@@ -2,5 +2,4 @@ currencylist = {'hkd':'$','cny': '￥','usd':'$','eur':'€'}
 core_currency = "HKD"
 
 block_explorer= 'https://blocks.bitcoin.org.hk/tx/'
-satwatch= ""
-dca = ""
+satwatch= "https://sats.bitcoin.org.hk"
